89f8eb95357d4a1a53160587bc200bb3eeee0b4e
sadiq <sadiqrauf00928@gmail.com>
2025-07-23 13:15:41 -0400

    feat/ tailwind css added

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div>
+            <div className=" bg-blue-500 h-20 text-white p-4 rounded font-sans">
+                Tailwind is working!
+            </div>
+            <h1 className="text-2xl font-roboto font-bold">Welcome to My Portfolio</h1>
+            <p>This is the home page of my portfolio website.</p>
+            <p>Feel free to explore my projects and skills.</p>
+        </div>
+    )
+}
