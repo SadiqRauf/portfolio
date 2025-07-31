04c9acdbbce14b71ebc2cd47b0815d70c80b08b9
sadiq <sadiqrauf00928@gmail.com>
2025-07-31 11:34:15 -0400

    fix/ added new fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css'
-
+import { AppWrapper } from './Components/common/PageMeta'
+import { ScrollToTop } from './Components/common/ScrollToTop'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 
 function App() {
 
     return (
-        <>
-            <Home />
-        </>
+        <AppWrapper>
+            <Router>
+
+                <ScrollToTop />
+                <Routes>
+                    {/* Landing Page Logic */}
+                    <Route path="/portfolio" element={<Home />} />
+
+                    {/* Fallback */}
+                    {/* <Route path="*" element={<NotFound />} /> */}
+                </Routes>
+            </Router>
+
+        </AppWrapper>
     )
 }
 
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,15 @@
+import Header from "../Components/Shared/Header/iindex";
+
+
 export default function Home() {
     return (
-        <div>
-            <div className=" bg-blue-500 h-20 text-white p-4 rounded font-sans">
-                Tailwind is working!
+        <div className=" relative h-[92vh] w-full grid grid-cols-12">
+
+            <div className=" h-20 w-20 bg-red-500">
+                <button>Click Me</button>
             </div>
-            <h1 className="text-2xl font-roboto font-bold">Welcome to My Portfolio</h1>
-            <p>This is the home page of my portfolio website.</p>
-            <p>Feel free to explore my projects and skills.</p>
+
+
         </div>
     )
 }
